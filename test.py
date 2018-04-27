8d1eccd5850418a8d170cc769e498ca4d1f20be0
odluna <38793638+odluna@users.noreply.github.com>
2018-04-27 13:47:35 -0400

    Update test.py

--- test.py
+++ test.py
@@ -3,10 +3,11 @@ import printer, textwrap, sys
 
 
  # Pi 1 and 2 
-   p = printer.ThermalPrinter(serialport="/dev/ttyAMA0")
+   
+  # p = printer.ThermalPrinter(serialport="/dev/ttyAMA0")
  
   # Pi 3
-  #  p = printer.ThermalPrinter(serialport="/dev/ttyS0")
+  p = printer.ThermalPrinter(serialport="/dev/ttyS0")
 
 unwrapped_text = "\n From My Pi, Roses are red, violets are blue, I'm seeing a future filled with Poems and you!\n"
 wrapped_text = textwrap.fill(unwrapped_text, 32) 
